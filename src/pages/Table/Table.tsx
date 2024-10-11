872cd3877f0666de90c1c920a07e7ea369d61c44
electra777 <electra0789@gmail.com>
2024-10-12 02:27:11 +0300

    added Table

---
+++ src/pages/Table/Table.tsx
@@ -0,0 +1,19 @@
+import styles from './Table.module.scss';
+
+const Table = () => {
+	return (
+		<div className={styles.table}>
+			<div className={styles.tableItem}>
+				<div className={styles.type}>Тип</div>
+				<div className={styles.time}>Время</div>
+				<div className={styles.employee}>Сотрудник</div>
+				<div className={styles.call}>Звонок</div>
+				<div className={styles.sourse}>Источник</div>
+				<div className={styles.grade}>Оценка</div>
+				<div className={styles.duration}>Длительность</div>
+			</div>
+		</div>
+	);
+};
+
+export default Table;
