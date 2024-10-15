4ea9e76c1678c7fe8938a8a04786433ae79e8ea3
electra777 <electra0789@gmail.com>
2024-10-16 02:24:39 +0300

    fix: add row, styless for last-of-type & change max-width in container

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,7 +11,7 @@ import styles from './Table.module.scss';
 const Table = () => {
 	return (
 		<div className={styles.table}>
-			<div className={styles.tableItem}>
+			<div className={styles.row}>
 				<div className={styles.type}>Тип</div>
 				<div className={styles.time}>Время</div>
 				<div className={styles.employee}>Сотрудник</div>
@@ -20,7 +20,7 @@ const Table = () => {
 				<div className={styles.grade}>Оценка</div>
 				<div className={styles.duration}>Длительность</div>
 			</div>
-			<div className={styles.tableItem}>
+			<div className={styles.row}>
 				<Type />
 				<Time />
 				<Employee />
@@ -28,7 +28,6 @@ const Table = () => {
 				<Source />
 				<Grade />
 				<Duration />
-
 			</div>
 		</div>
 	);
